c41fc462dd808002d497f81c04058825ee1a056f
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-02 00:27:39 +0300

    Добавил аннотации Swagger в модели api

--- src/main/java/ru/bobrov/vyacheslav/chat/controllers/models/response/ChatApiModel.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/controllers/models/response/ChatApiModel.java
@@ -1,5 +1,7 @@
 package ru.bobrov.vyacheslav.chat.controllers.models.response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
@@ -9,11 +11,18 @@ import java.util.UUID;
 @Value
 @Builder
 @NonNull
+@ApiModel
 public class ChatApiModel {
+    @ApiModelProperty
     UUID chatId;
+    @ApiModelProperty
     String title;
+    @ApiModelProperty (allowableValues = "ACTIVE, DISABLED")
     String status;
+    @ApiModelProperty
     String created;
+    @ApiModelProperty
     String updated;
+    @ApiModelProperty
     UserApiModel creator;
 }
--- src/main/java/ru/bobrov/vyacheslav/chat/controllers/models/response/UsersPagingApiModel.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/controllers/models/response/UsersPagingApiModel.java
@@ -1,5 +1,7 @@
 package ru.bobrov.vyacheslav.chat.controllers.models.response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
@@ -11,10 +13,16 @@ import java.util.UUID;
 @Value
 @Builder
 @NonNull
+@ApiModel
 public class UsersPagingApiModel {
+    @ApiModelProperty
     List<UUID> ids;
+    @ApiModelProperty
     Map<UUID, UserApiModel> items;
+    @ApiModelProperty
     Integer page;
+    @ApiModelProperty
     Integer pageLimit;
+    @ApiModelProperty
     Long totalItems;
 }
