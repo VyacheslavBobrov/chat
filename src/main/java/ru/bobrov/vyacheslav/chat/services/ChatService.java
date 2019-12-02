02cd6fdf3a18d6311e408f49a3789ad38993df19
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-02 12:04:36 +0300

    Добавил коментарии к api сервиса чатов

--- src/main/java/ru/bobrov/vyacheslav/chat/services/ChatService.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/services/ChatService.java
@@ -21,6 +21,9 @@ import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PUBLIC;
 import static ru.bobrov.vyacheslav.chat.services.Utils.*;
 
+/**
+ * Сервис для работы с сущностью Chat
+ */
 @Service
 @AllArgsConstructor(access = PUBLIC)
 @FieldDefaults(level = PRIVATE)
@@ -29,10 +32,23 @@ public class ChatService {
     @NonNull ChatRepository repository;
     @NonNull UserService userService;
 
+    /**
+     * Получить чат по идентификатору
+     *
+     * @param uuid уникальный идентификатор чата
+     * @return {@link Chat} созданный чат
+     */
     public Chat get(UUID uuid) {
         return repository.findById(uuid).orElseThrow(ChatNotFoundException::new);
     }
 
+    /**
+     * Создать новый чат
+     *
+     * @param title   заголовок чата
+     * @param creator {@link UUID} уникальный идентификатор создателя чата
+     * @return {@link Chat} созданный чат
+     */
     public Chat create(
             String title,
             UUID creator
@@ -53,6 +69,13 @@ public class ChatService {
         return repository.save(chat);
     }
 
+    /**
+     * Обновить данные чата
+     *
+     * @param chatId {@link UUID} идентификатор чата
+     * @param title  Новый заголовок чата
+     * @return {@link Chat} обновленный чат
+     */
     public Chat update(UUID chatId, String title) {
         Chat chat = get(chatId);
         chat.setTitle(title);
@@ -61,12 +84,24 @@ public class ChatService {
         return repository.save(chat);
     }
 
+    /**
+     * Обновить время изменения чата (при добавлении нового сообщения)
+     *
+     * @param chatId {@link UUID} идентификатор чата
+     */
     public void update(UUID chatId) {
         Chat chat = get(chatId);
         updateTime(chat);
         repository.save(chat);
     }
 
+    /**
+     * Добавить пользователей в чат
+     *
+     * @param chatId    {@link UUID} идентификатор чата
+     * @param userUUIDs {@link List<UUID>} список идентификаторов пользователей
+     * @return {@link Set<User>} пользователи, добавленные в чат
+     */
     public Set<User> addUsers(UUID chatId, Collection<UUID> userUUIDs) {
         Chat chat = get(chatId);
         chat.getUsers().addAll(userService.get(userUUIDs));
@@ -74,10 +109,22 @@ public class ChatService {
         return chat.getUsers();
     }
 
+    /**
+     * Блокировать чат
+     *
+     * @param uuid {@link UUID} идентификатор чата
+     * @return {@link Chat} заблокированный чат
+     */
     public Chat block(UUID uuid) {
         return setChatStatus(uuid, ChatStatus.DISABLED);
     }
 
+    /**
+     * Разблокировать чат
+     *
+     * @param uuid {@link UUID} идентификатор чата
+     * @return {@link Chat} разблокированный чат
+     */
     public Chat unblock(UUID uuid) {
         return setChatStatus(uuid, ChatStatus.ACTIVE);
     }
@@ -90,10 +137,24 @@ public class ChatService {
         return chat;
     }
 
+    /**
+     * Получить всех пользователей чата
+     *
+     * @param uuid {@link UUID} идентификатор чата{@link UUID} идентификатор чата
+     * @return {@link Set<User>} пользователи чата
+     */
     public Set<User> getChatUsers(UUID uuid) {
         return get(uuid).getUsers();
     }
 
+    /**
+     * Получить сообщения чата (с пагинацией)
+     *
+     * @param uuid {@link UUID} идентификатор чата{@link UUID} идентификатор чата
+     * @param page номер страницы
+     * @param size размер страницы
+     * @return {@link Page<Message>} порция сообщений чата
+     */
     public Page<Message> getChatMessages(UUID uuid, int page, int size) {
         List<Message> messages = new ArrayList<>(get(uuid).getMessages());
         messages.sort(Comparator.comparing(Message::getCreated).reversed());
