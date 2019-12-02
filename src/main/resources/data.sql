b95552d952ce726d8ad5c1919aa458b656b9bd88
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-02 18:58:55 +0300

    Изменил пароль администратора

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,4 +7,11 @@ MERGE INTO USERS (
     user_role
 ) KEY (user_id)
 VALUES
-  ('4d6b8d54-3d13-40ed-978b-8e71a6abc58b', 'ADMIN', 'admin', 'set password', 'ACTIVE', 'ADMIN');
+  (
+    '4d6b8d54-3d13-40ed-978b-8e71a6abc58b',
+    'ADMIN',
+    'admin',
+    '$2a$10$lP9EJ9JrU8pDNUTi9GijReKQnF6svn5Fs0DLIjMU0luwRk4zWK4UC',
+    'ACTIVE',
+    'ADMIN'
+  );
