d573739371542c81d5dc071bd259e87dd8c2a3fe
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-04 10:39:03 +0300

    Отрефакторил исключения - удалил ненужные аннотации

--- src/main/java/ru/bobrov/vyacheslav/chat/dataproviders/exceptions/ChatNotFoundException.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/dataproviders/exceptions/ChatNotFoundException.java
@@ -1,15 +1,5 @@
 package ru.bobrov.vyacheslav.chat.dataproviders.exceptions;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.FieldDefaults;
-
-@Data
-@AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@EqualsAndHashCode(callSuper = true)
 public class ChatNotFoundException extends ResourceNotFoundException {
     public ChatNotFoundException(String title, String message) {
         super(title, message);
