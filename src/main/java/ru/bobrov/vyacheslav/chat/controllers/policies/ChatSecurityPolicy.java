d91b8a3f8e3e20ce37b639f4b9fa4b5d7d778d70
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-05 12:10:38 +0300

    Запретил создателю чата выбрасывать из него админа

--- src/main/java/ru/bobrov/vyacheslav/chat/controllers/policies/ChatSecurityPolicy.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/controllers/policies/ChatSecurityPolicy.java
@@ -6,13 +6,14 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import ru.bobrov.vyacheslav.chat.dataproviders.entities.Chat;
+import ru.bobrov.vyacheslav.chat.dataproviders.entities.UserRole;
 import ru.bobrov.vyacheslav.chat.services.ChatService;
+import ru.bobrov.vyacheslav.chat.services.UserService;
 
 import java.util.UUID;
 
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PUBLIC;
-import static ru.bobrov.vyacheslav.chat.controllers.policies.Util.isAdmin;
 import static ru.bobrov.vyacheslav.chat.controllers.policies.Util.isCurrentUserId;
 
 @Component
@@ -22,30 +23,38 @@ import static ru.bobrov.vyacheslav.chat.controllers.policies.Util.isCurrentUserI
 @SuppressWarnings("unused")
 public class ChatSecurityPolicy {
     ChatService chatService;
+    UserService userService;
 
     public boolean canBlockOrUnblockChat(@NonNull UserDetails principal, @NonNull UUID chatId) {
         //Заблокировать и разблокировать чат может админ и создатель чата
-        return isAdmin(principal) || isChatCreator(principal, chatId);
+        return Util.isAdmin(principal) || isChatCreator(principal, chatId);
     }
 
     public boolean canReadChat(@NonNull UserDetails principal, @NonNull UUID chatId) {
         //Данные чата может читать админ и участники чата
-        return isAdmin(principal) || chatService.getChatUsers(chatId)
+        return Util.isAdmin(principal) || chatService.getChatUsers(chatId)
                 .stream().anyMatch(user -> isCurrentUserId(principal, user.getUserId()));
     }
 
     public boolean canUpdateChat(@NonNull UserDetails principal, @NonNull UUID chatId) {
         //Обновлять данные чата могут админ и создатель чата
-        return isAdmin(principal) || isChatCreator(principal, chatId);
+        return Util.isAdmin(principal) || isChatCreator(principal, chatId);
     }
 
     public boolean canKickUser(@NonNull UserDetails principal, @NonNull UUID chatId, @NonNull UUID userId) {
-        //Из чата могут выбросить пользователя: админ, создатель чата, и сам пользователь.
-        return isAdmin(principal) || isChatCreator(principal, chatId) || isCurrentUserId(principal, userId);
+        //Из чата могут выбросить пользователя: админ, создатель чата, и сам пользователь,
+        // пользователь не может выбросить админа.
+        return Util.isAdmin(principal)
+                || isCurrentUserId(principal, userId)
+                || (isChatCreator(principal, chatId) && !isAdmin(userId));
     }
 
     public boolean isChatCreator(UserDetails principal, UUID chatId) {
         Chat chat = chatService.get(chatId);
         return isCurrentUserId(principal, chat.getCreator().getUserId());
+    }
+
+    private boolean isAdmin(UUID userId) {
+        return userService.get(userId).getRole().equals(UserRole.ADMIN);
     }
 }
