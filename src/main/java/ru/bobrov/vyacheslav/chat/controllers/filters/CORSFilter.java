c50a4dcef7ae65eb0efa1c29f6e420d89a5ff761
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-08 14:01:07 +0300

    Перенес логику ответа на Pre-flight запрос в CORSFilter

--- src/main/java/ru/bobrov/vyacheslav/chat/controllers/filters/CORSFilter.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/controllers/filters/CORSFilter.java
@@ -1,22 +1,56 @@
 package ru.bobrov.vyacheslav.chat.controllers.filters;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
+    static final String[] ALLOW_HEADERS = {
+            "x-requested-with",
+            "Content-Type",
+            "Access-Control-Request-Method",
+            "Access-Control-Request-Headers",
+            "Authorization",
+            "Access-Control-Allow-Origin",
+            "Access-Control-Expose-Headers",
+            "accept",
+            "observe",
+            "origin",
+            "responseType",
+            "x-requested-with",
+    };
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String origin = request.getHeader("Origin");
+
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            log.info("Pre-flight: " + origin);
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Vary", "Origin");
+            response.setHeader("Access-Control-Allow-Headers", String.join(",", ALLOW_HEADERS));
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        log.info("CORS: " + origin);
+        response.setHeader("Access-Control-Allow-Origin", origin); //"http://127.0.0.1:8080");//"http://localhost:8081");
+        response.setHeader("Vary", "Origin");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", String.join(",", ALLOW_HEADERS));
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         chain.doFilter(req, res);
--- src/main/java/ru/bobrov/vyacheslav/chat/controllers/filters/JwtRequestFilter.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/controllers/filters/JwtRequestFilter.java
@@ -1,110 +1,33 @@
 package ru.bobrov.vyacheslav.chat.controllers.filters;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.bobrov.vyacheslav.chat.configurations.JwtTokenUtil;
-import ru.bobrov.vyacheslav.chat.services.JwtUserDetailsService;
+import ru.bobrov.vyacheslav.chat.services.utils.JwtAuthenticationService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static java.lang.String.format;
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PUBLIC;
-import static ru.bobrov.vyacheslav.chat.services.Constants.AUTHORIZATION_HEADER;
-import static ru.bobrov.vyacheslav.chat.services.Constants.TOKEN_PREFIX;
 
 @Component
 @AllArgsConstructor(access = PUBLIC)
 @FieldDefaults(level = PRIVATE)
 @Slf4j
 public class JwtRequestFilter extends OncePerRequestFilter {
-    static final Pattern TOKEN_PATTERN = Pattern.compile(format("^%s\\s+(\\S*)$", TOKEN_PREFIX));
-    static final String[] ALLOW_HEADERS = {
-            "Access-Control-Expose-Headers",
-            "Authorization",
-            "accept",
-            "access-control-request-headers",
-            "access-control-request-method",
-            "content-type",
-            "observe",
-            "origin",
-            "responseType",
-            "x-requested-with"
-    };
-
-    @NonNull JwtUserDetailsService jwtUserDetailsService;
-    @NonNull JwtTokenUtil jwtTokenUtil;
-
-    private void authenticate(HttpServletRequest request) {
-        final String tokenHeader = request.getHeader(AUTHORIZATION_HEADER);
-        if (tokenHeader == null)
-            return;
-
-        final String token = getTokenFromHeader(tokenHeader);
-        if (token == null) {
-            logger.warn("JWT Token does not begin with Bearer String");
-            return;
-        }
-
-        try {
-            setContext(request, token);
-        } catch (IllegalArgumentException e) {
-            log.error("Unable to get JWT Token", e);
-        } catch (ExpiredJwtException e) {
-            SecurityContextHolder.clearContext();
-            log.error("JWT Token has expired", e);
-        }
-    }
-
-    private void setContext(final HttpServletRequest request, final String token) {
-        final String username = jwtTokenUtil.getUsernameFromToken(token);
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
-
-            if (jwtTokenUtil.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }
-        }
-    }
-
-    private String getTokenFromHeader(String header) {
-        Matcher matcher = TOKEN_PATTERN.matcher(header);
-        return matcher.find() && matcher.groupCount() == 1 ? matcher.group(1) : null;
-    }
+    @NonNull JwtAuthenticationService authenticationService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
-        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
-            log.info("Pre-flight");
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Headers", String.join(",", ALLOW_HEADERS));
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setStatus(HttpServletResponse.SC_OK);
-            return;
-        }
-        authenticate(request);
+        authenticationService.authenticate(request);
         chain.doFilter(request, response);
     }
 }
