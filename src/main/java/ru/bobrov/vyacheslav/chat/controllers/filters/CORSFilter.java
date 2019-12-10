054582d7e75f77239dcb0cc21b8ad2a3d8acb472
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-10 22:15:50 +0300

    Отрефакторил CORSFilter - использовал константы с именами заголовков и методов

--- src/main/java/ru/bobrov/vyacheslav/chat/controllers/filters/CORSFilter.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/controllers/filters/CORSFilter.java
@@ -1,57 +1,69 @@
 package ru.bobrov.vyacheslav.chat.controllers.filters;
 
+import com.google.common.net.HttpHeaders;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpMethod.*;
 
 @Component
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
-    static final String[] ALLOW_HEADERS = {
-            "x-requested-with",
-            "Content-Type",
-            "Access-Control-Request-Method",
-            "Access-Control-Request-Headers",
-            "Authorization",
-            "Access-Control-Allow-Origin",
-            "Access-Control-Expose-Headers",
-            "accept",
+    static final String[] ALLOWED_HEADERS = {
+            CONTENT_TYPE,
+            HttpHeaders.X_REQUESTED_WITH,
+            ACCESS_CONTROL_REQUEST_METHOD,
+            ACCESS_CONTROL_REQUEST_HEADERS,
+            AUTHORIZATION,
+            ACCESS_CONTROL_ALLOW_ORIGIN,
+            ACCESS_CONTROL_EXPOSE_HEADERS,
+            ACCEPT,
             "observe",
-            "origin",
+            ORIGIN,
             "responseType",
-            "x-requested-with",
     };
+
+    static final List<String> ALLOWED_METHODS = Stream.of(
+            POST, GET, OPTIONS, PUT, DELETE
+    ).map(HttpMethod::name).collect(Collectors.toUnmodifiableList());
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        String allowedOrigin = request.getHeader("Origin"); //TODO: на время разработки, в проде заменить на адрес фронта
+        String allowedOrigin = request.getHeader(ORIGIN); //TODO: на время разработки, в проде заменить на адрес фронта
 
-        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+        if (request.getMethod().equalsIgnoreCase(OPTIONS.name())) {
             log.info("Pre-flight: " + allowedOrigin);
-            response.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-            response.setHeader("Vary", "Origin");
-            response.setHeader("Access-Control-Allow-Headers", String.join(",", ALLOW_HEADERS));
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setStatus(HttpServletResponse.SC_OK);
+            response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, allowedOrigin);
+            response.setHeader(VARY, ORIGIN);
+            response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, String.join(",", ALLOWED_HEADERS));
+            response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, Boolean.TRUE.toString());
+            response.setStatus(SC_OK);
             return;
         }
 
         log.info("CORS: " + allowedOrigin);
-        response.setHeader("Access-Control-Allow-Origin", allowedOrigin); //"http://127.0.0.1:8080");//"http://localhost:8081");
-        response.setHeader("Vary", "Origin");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", String.join(",", ALLOW_HEADERS));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, allowedOrigin);
+        response.setHeader(VARY, ORIGIN);
+        response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, String.join(",", ALLOWED_METHODS));
+        response.setHeader(ACCESS_CONTROL_MAX_AGE, "3600");
+        response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, String.join(",", ALLOWED_HEADERS));
+        response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, Boolean.TRUE.toString());
 
         chain.doFilter(req, res);
     }
