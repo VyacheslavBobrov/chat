c68c90462feabbfb56c31a5146d554e91bb8e0c1
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-12-11 21:00:34 +0300

    Изменил формат сообщения веб сокета для пользователя

--- src/main/java/ru/bobrov/vyacheslav/chat/services/websocket/ChatListNotifyService.java
+++ src/main/java/ru/bobrov/vyacheslav/chat/services/websocket/ChatListNotifyService.java
@@ -3,6 +3,7 @@ package ru.bobrov.vyacheslav.chat.services.websocket;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import ru.bobrov.vyacheslav.chat.services.ChatService;
@@ -20,33 +21,35 @@ import static ru.bobrov.vyacheslav.chat.services.websocket.events.ChatListEvent.
 @AllArgsConstructor(access = PUBLIC)
 @FieldDefaults(level = PRIVATE)
 @NonNull
+@Slf4j
 public class ChatListNotifyService {
     SimpMessagingTemplate messagingTemplate;
     ChatService chatService;
 
     public void newChat(UUID chatId) {
-        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getLogin(), NEW_CHAT, chatId));
+        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getUserId(), NEW_CHAT, chatId));
     }
 
     public void chatBlocked(UUID chatId) {
-        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getLogin(), BLOCKED_CHAT, chatId));
+        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getUserId(), BLOCKED_CHAT, chatId));
     }
 
     public void chatUnblocked(UUID chatId) {
-        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getLogin(), UNBLOCKED_CHAT, chatId));
+        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getUserId(), UNBLOCKED_CHAT, chatId));
     }
 
     public void nameChanged(UUID chatId) {
-        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getLogin(), RENAMED_CHAT, chatId));
+        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getUserId(), RENAMED_CHAT, chatId));
     }
 
     public void newMessageInChat(UUID chatId) {
-        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getLogin(), NEW_MESSAGE, chatId));
+        chatService.applyForChatUsers(chatId, user -> sendMessage(user.getUserId(), NEW_MESSAGE, chatId));
     }
 
-    private void sendMessage(String login, Type type, UUID uuid) {
+    private void sendMessage(UUID userId, Type type, UUID uuid) {
+        log.info("Send message " + type + " for " + userId);
         messagingTemplate.convertAndSendToUser(
-                login,
+                userId.toString(),
                 CHANNEL,
                 ChatListEvent.builder().uuid(uuid).type(type).build()
         );
